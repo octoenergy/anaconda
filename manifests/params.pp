991ce2a0e9e997ff206e537af91ce66d9ad4c911
David Winterbottom <david.winterbottom@gmail.com>
2018-01-05 21:01:57 +0000

    Upgrade to 5.0.1

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,14 +5,14 @@ class anaconda::params {
       default           =>"http://repo.continuum.io/archive",
     }
 
+    $version = "5.0.1"
+
     $installer = $::kernel ? {
-      /(L|l)inux/ => "Anaconda3-4.4.0-Linux-x86_64.sh",
-      'windows'   => "Anaconda-4.4.0-Windows-x86_64.exe",
-      'Darwin'    => "Anaconda3-4.4.0-MacOSX-x86_64.sh",
+      /(L|l)inux/ => "Anaconda3-$version-Linux-x86_64.sh",
+      'windows'   => "Anaconda-$version-Windows-x86_64.exe",
+      'Darwin'    => "Anaconda3-$version-MacOSX-x86_64.sh",
     }
     #TODO: add support for other versions
 
     $url = "${base_url}/${installer}"
 }
-
-
